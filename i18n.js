150d63d15ce63e491a6d4bbc2653305a47e0e6ff
Stijn de Witt <StijnDeWitt@hotmail.com>
2018-03-29 15:06:20 +0200

    1.7.1: Removed no-parse path from webpack loader, added browser main entry.

--- i18n.js
+++ i18n.js
@@ -1,4 +1,4 @@
-var I18n = require('i18nline/lib/i18n');
+var I18n = require('i18nline');
 var extend = require('./dist/extensions/i18n_js');
 
 module.exports = extend(I18n);
--- webpack-loader.js
+++ webpack-loader.js
@@ -5,33 +5,11 @@ var config = I18nline.config;
 var preprocess = require("./preprocess");
 var hasTranslatableText = require("./hasTranslatableText")(config);
 
-/*
- * there's code in this file that dynamically requires plugins; it's
- * not needed in the browser, so we skip it ... otherwise webpack
- * will load *all* of i18nline (which includes things like fs, and
- * will fail).
- */
-var noParsePath = "i18nline/lib/i18nline";
-var addNoParse = function() {
-  var escapeRegExp = require("./util/escapeRegExp");
-  var path = require("path");
-  var mod = this.options.module;
-
-  mod.noParse = mod.noParse || [];
-  if (!Array.isArray(mod.noParse))
-    mod.noParse = [mod.noParse];
-  mod.noParse.push(new RegExp(escapeRegExp(path.normalize(noParsePath))));
-
-  addNoParse = Function.prototype;
-};
-
 module.exports = function(source) {
   this.cacheable();
-  addNoParse.call(this);
   if (hasTranslatableText(source))
     source = preprocess(source, config);
   return source;
 };
 
 log.log('Initialized ' + log.name);
-
